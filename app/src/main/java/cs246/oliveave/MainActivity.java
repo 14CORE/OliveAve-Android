1e38739d8091934110af358af6304b880005f69c
Kevin <liz11002@byui.edu>
2017-02-15 22:46:02 -0700

    changed name of logo.jpg

--- app/src/main/java/cs246/oliveave/MainActivity.java
+++ app/src/main/java/cs246/oliveave/MainActivity.java
@@ -3,6 +3,8 @@ package cs246.oliveave;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import cs246.oliveave.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
