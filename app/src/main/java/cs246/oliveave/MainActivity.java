2cec26d91306a7e8cfdd76fca622521ac45d79eb
aerichsenf <aerichsen@byui.edu>
2017-02-22 13:21:31 -0700

    addding onClickListener

--- app/src/main/java/cs246/oliveave/MainActivity.java
+++ app/src/main/java/cs246/oliveave/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 
 import cs246.oliveave.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +18,12 @@ public class MainActivity extends AppCompatActivity {
     public void adminBtn(View view){
         Intent i = new Intent(this, AdminMenu.class);
         startActivity(i);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        // Add buttons here
 
     }
 }
