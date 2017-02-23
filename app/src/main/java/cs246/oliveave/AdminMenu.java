161e4aa9f7334abb262adcd1c585e977f67fd399
aerichsenf <aerichsen@byui.edu>
2017-02-23 14:04:18 -0700

    Authentication is now working

--- app/src/main/java/cs246/oliveave/AdminMenu.java
+++ app/src/main/java/cs246/oliveave/AdminMenu.java
@@ -3,12 +3,12 @@ package cs246.oliveave;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
 
 public class AdminMenu extends AppCompatActivity {
 
-    private DatabaseReference mDatabase;
+  //  private DatabaseReference mDatabase;
 
 
     @Override
