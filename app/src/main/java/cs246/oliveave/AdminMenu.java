d8510f22e5775175bd073179956972727f3bfb5d
Kevin <liz11002@byui.edu>
2017-02-23 19:49:14 -0700

    New merge to master repo. New activity for admin

--- app/src/main/java/cs246/oliveave/AdminMenu.java
+++ app/src/main/java/cs246/oliveave/AdminMenu.java
@@ -1,25 +1,86 @@
 package cs246.oliveave;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AdminMenu extends AppCompatActivity {
 
-  //  private DatabaseReference mDatabase;
-
+    Button confirmBtn;
+    Button clearBtn;
+    EditText userInput;
+    //private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_menu);
 
-        /*FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-        myRef.setValue("Hello, world!");*/
+        clearBtn = (Button)findViewById(R.id.clearUserID);
+        confirmBtn = (Button) findViewById(R.id.emailContinue);
+        userInput = (EditText) findViewById(R.id.userEmail);
+
+        checkValidation();
+
+        userInput.addTextChangedListener(mWatcher);
+
+
     }
 
-    //mDatabase = FirebaseDatabase.getInstance().getReference();
+
+    //validation for text in input
+    private void checkValidation() {
+        // TODO Auto-generated method stub
+
+        if ((TextUtils.isEmpty(userInput.getText())))
+            clearBtn.setEnabled(false);
+        else
+            clearBtn.setEnabled(true);
+
+    }
+
+    //checks for user input to enable the clear button
+    TextWatcher mWatcher = new TextWatcher() {
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before,
+                                  int count) {
+            // TODO Auto-generated method stub
+            checkValidation();
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count,
+                                      int after) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            // TODO Auto-generated method stub
+
+        }
+    };
+
+
+
+    public void continueToAdminOptions(View view){
+        Intent intent = new Intent(this, PointsActivity.class);
+        startActivity(intent);
+    }
+
+    public void clearUserInput(View view){
+        userInput.setText("");
+    }
 }
