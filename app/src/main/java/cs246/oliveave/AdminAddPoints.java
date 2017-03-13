7fcbe143274d4acb91cf68c267cd0d8f1041788e
Kevin <liz11002@byui.edu>
2017-03-13 13:20:32 -0600

    Admin activities

--- app/src/main/java/cs246/oliveave/AdminAddPoints.java
+++ app/src/main/java/cs246/oliveave/AdminAddPoints.java
@@ -1,43 +1,86 @@
 package cs246.oliveave;
 
 import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AdminAddPoints extends AppCompatActivity {
 
     TextView userDisplay;
     String userEmail;
-
+    String userKey;
+    private  DatabaseReference _databaseRef;
+    private FirebaseAuth _authDb;
+    TextView _userName;
+    TextView _userPoints;
+    User customer;
+    int points;
+    String mPoints;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_points);
-        userDisplay = (TextView) findViewById(R.id.welcomeUser);
-        Intent intent = getIntent();
-        //userEmail = intent.getExtras().getString(userEmail);
+        Bundle extras = getIntent().getExtras();
 
-        userDisplay.setText(userEmail);
+        _userName = (TextView)findViewById(R.id.userName);
+        _userPoints = (TextView)findViewById(R.id.userPoints);
 
+        if(extras == null){
+            userKey = null;
+        }else{
+            userKey = extras.getString("UserUid");
+        }
+        _databaseRef = FirebaseDatabase.getInstance().getReferenceFromUrl("https://oliveavecs246.firebaseio.com/Users/" + userKey);
+        _authDb = FirebaseAuth.getInstance();
     }
 
+    @Override
+    protected void onStart(){
+        super.onStart();
+        _databaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                customer = dataSnapshot.getValue(User.class);
+                _userName.setText(customer.getName());
+                _userPoints.setText(customer.getPoints()+" points");
+                mPoints = customer.getPoints();
+            }
 
-    public void addPointsToUser(){
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
+            }
+
+
+        });
     }
 
-    public void redeemPointsFromUser(){
-
+    public void addPoints(View view){
+        points = Integer.parseInt(customer.getPoints());
+        points++;
+        mPoints = String.valueOf(points);
+        _databaseRef.child("points").setValue(mPoints);
     }
 
-    public void cancelTransaction(){
+    public void redeemPoints(View view){
+        _databaseRef.child("points").setValue("0");
+    }
+
+    public void cancelTransaction(View view){
 
     }
 }
--- app/src/main/java/cs246/oliveave/AdminSignIn.java
+++ app/src/main/java/cs246/oliveave/AdminSignIn.java
@@ -14,13 +14,16 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AdminSignIn extends AppCompatActivity {
-
+    private DatabaseReference myFirebaseRef;
     private EditText adminUsername, adminPwd;
     //private Button signIn;
     private FirebaseAuth adminAuth;
-
+    private FirebaseUser mUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +31,10 @@ public class AdminSignIn extends AppCompatActivity {
 
         adminUsername = (EditText)findViewById(R.id.adminUsername);
         adminPwd = (EditText) findViewById(R.id.adminPassword);
+        myFirebaseRef = FirebaseDatabase.getInstance().getReferenceFromUrl("https://oliveavecs246.firebaseio.com/Users");
+
         adminAuth = FirebaseAuth.getInstance();
+        //       mUser = adminAuth.getCurrentUser();
     }
 
     public void adminSignIn(View view){
@@ -47,18 +53,11 @@ public class AdminSignIn extends AppCompatActivity {
         adminAuth.signInWithEmailAndPassword(_email,_password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if(!task.isSuccessful()){
-                    if(_password.length() < 4){
-                        adminPwd.setError("Password has to be at least 4 characters.");
-                    }
-                    else{
-                        Toast.makeText(getApplicationContext(), "WRONG PASSWORD", Toast.LENGTH_SHORT).show();
-                    }
-                }
-                else{
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"SUCCESSFULLY ENTER IN THE DB", Toast.LENGTH_SHORT).show();
                     Intent i = new Intent(AdminSignIn.this, AdminMenu.class);
                     startActivity(i);
-                    finish();
+
                 }
             }
         });
